59f71668f0bc121d3021fd03f71282790d234e42
wahahahhaya <a870128rlen@gmail.com>
2025-11-18 08:43:57 +0000

    Add installation commands and download instructions for Hugging Face models

--- test_offline.sh
+++ test_offline.sh
@@ -1,3 +1,11 @@
+""
+pip install -U transformers accelerate torch triton==3.4 kernels
+pip install "huggingface_hub>=0.34.0,<1.0.0"
+hf --help  # 確認有這個指令
+hf download openai/gpt-oss-20b \
+  --local-dir gpt-oss-20b
+""
+
 export HF_HUB_OFFLINE=1 # 設定離線模式
 
 python gpt-oss.py
