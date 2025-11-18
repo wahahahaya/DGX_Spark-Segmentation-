307fc782997da4d003aef259c68a2cf348c75d9a
wahahahhaya <a870128rlen@gmail.com>
2025-11-18 08:54:41 +0000

    Implement dino.py script for image annotation using GroundingDINO model

---
+++ dino.py
@@ -0,0 +1,21 @@
+from GroundingDINO.groundingdino.util.inference import load_model, load_image, predict, annotate
+import cv2
+
+model = load_model("GroundingDINO/groundingdino/config/GroundingDINO_SwinT_OGC.py", "GroundingDINO/weights/groundingdino_swint_ogc.pth")
+IMAGE_PATH = "GroundingDINO/weights/dog-3.jpeg" #還沒下載圖片
+TEXT_PROMPT = "chair . person . dog ."
+BOX_TRESHOLD = 0.35
+TEXT_TRESHOLD = 0.25
+
+image_source, image = load_image(IMAGE_PATH)
+
+boxes, logits, phrases = predict(
+    model=model,
+    image=image,
+    caption=TEXT_PROMPT,
+    box_threshold=BOX_TRESHOLD,
+    text_threshold=TEXT_TRESHOLD
+)
+
+annotated_frame = annotate(image_source=image_source, boxes=boxes, logits=logits, phrases=phrases)
+cv2.imwrite("dino_demo/annotated_image.jpg", annotated_frame)
